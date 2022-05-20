08b8797009f70894cdc44f614c51db380e2b31de
barti132 <barteksredzinski@gmail.com>
2022-05-20 22:33:46 +0200

    use billboard.py, downloaded billboard-data.csv

--- billboard_scrap.py
+++ billboard_scrap.py
@@ -12,13 +12,19 @@ print("Start from: " + billboard_date.strftime('%Y-%m-%d'))
 
 url = "https://www.billboard.com/charts/hot-100/"
 
-billboard_pages = []
+billboard_data = []
 while today_date.date() > billboard_date.date():
     date = billboard_date.strftime('%Y-%m-%d')
     with urllib.request.urlopen(f'{url}{date}') as resp:
+        data = []
         processed_page = BeautifulSoup(resp.read().decode('utf-8'), "html.parser")
-        billboard_pages.append(processed_page)
-    billboard_date += relativedelta(months=+1)
+        
+        "c-tagline  a-font-primary-medium-xs u-font-size-11@mobile-max u-letter-spacing-0106 u-letter-spacing-0089@mobile-max lrv-u-line-height-copy lrv-u-text-transform-uppercase lrv-u-margin-a-00 lrv-u-padding-l-075 lrv-u-padding-l-00@mobile-max"
+        billboard_data.append(data)
+    billboard_date += relativedelta(months=+3) #change to full year
 
-print(len(billboard_pages))
+
+print(billboard_data[1])
+
+print(len(billboard_data))
 print("Ended: " + billboard_date.strftime('%Y-%m-%d'))
